4967cec340e7f95374803c237ca2c7502c9b15a3
Gustavo Sanchez <gustavosanchezgalarza@gmail.com>
2025-08-31 17:03:10 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+Victor Crest
+  26
+  London
+
+  80K
+  Followers
+
+  803K
+  Likes
+
+  1.4K
+  Photos
+    </>
+  )
+}
+
+export default App
